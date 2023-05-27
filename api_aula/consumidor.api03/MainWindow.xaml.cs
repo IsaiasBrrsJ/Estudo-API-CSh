040560157a0b20736c945d9fec8293ad9e8c4379
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2023-05-27 18:59:44 -0400

    API usando com vários sistemas

---
+++ api_aula/consumidor.api03/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using Flurl.Http;
+using Flurl;
+using System.Collections;
+using consumidor.api03.Model;
+using System.Collections.Generic;
+
+namespace consumidor.api03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAtualizar_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://localhost:7131/";
+            var ednpoint = url+"api/TarefaItems";
+
+            IEnumerable<Trabalho> listaTarefa =  ednpoint.GetJsonAsync<IEnumerable<Trabalho>>().Result;
+
+            dtgGrid.ItemsSource = listaTarefa;
+        }
+    }
+}
